6ccefc09004c299a6e37fd0362140062c1c3c488
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-07 19:31:29 -0500

    Complete the main and add configurations

--- OryxPro2020/battery-backlight/main.py
+++ OryxPro2020/battery-backlight/main.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
+from configparser import ConfigParser
 import os
 
 from battery import Battery
-from colors import GREEN, YELLOW, RED
 from keyboard_backlight import KeyboardBacklight
+
+CONFIGURATION_PATH = '/etc/battery-backlight.conf'
+
+
+def read_configurations():
+    if os.path.exists(CONFIGURATION_PATH):
+        config_parser = ConfigParser()
+        config_parser.read_file(CONFIGURATION_PATH)
+        return config_parser
+    return {}
 
 
 def main():
+    config = read_configurations()
+
     battery = Battery()
-    kb_backlight = KeyboardBacklight(mode='static', battery_handler=battery)
+    kb_backlight = KeyboardBacklight(
+        mode=config.get('mode', 'breath'),
+        battery_handler=battery
+    )
     kb_backlight.run()
 
 
