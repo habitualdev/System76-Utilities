af22ea107fa2c2ecad3277efd28b78319545250a
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-07 20:13:48 -0500

    Create first iteration of package

---
+++ OryxPro2020/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='s76-battery-backlight',
+    version='0.1',
+    description='Software to show your battery status with your keyboard backlight',
+    url='#',
+    author='Jean-François Labonté',
+    author_email='grimsleepless@protonmail.com',
+    license='Apache License 2.0',
+    packages=['battery-backlight'],
+)
