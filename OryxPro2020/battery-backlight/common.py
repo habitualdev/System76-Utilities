a29faf62c845df393ab743c3b748a68f72ce0f71
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-07 19:08:09 -0500

    Update common functions
    Handle possibility that the data can be a byte

--- OryxPro2020/battery-backlight/common.py
+++ OryxPro2020/battery-backlight/common.py
@@ -1,3 +1,9 @@
-def read_file(path):
+def read_file(path: str) -> str:
     with open(path, 'r') as f:
-        return f.read().decode('utf-8')
+        data = f.read()
+        return data if isinstance(data, str) else data.decode('utf-8')
+
+
+def write_file(path: str, value: str):
+    with open(path, 'wb') as f:
+        f.write(value.encode())
