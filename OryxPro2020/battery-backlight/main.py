4741e9cd4a6a0bc3077c671b58ad64ed77eff14f
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-07 19:09:30 -0500

    Update main file

--- OryxPro2020/battery-backlight/main.py
+++ OryxPro2020/battery-backlight/main.py
@@ -1,39 +1,16 @@
 #!/usr/bin/env python3
 import os
 
+from battery import Battery
 from colors import GREEN, YELLOW, RED
+from keyboard_backlight import KeyboardBacklight
 
-battery = 100
-brightness = 100
 
-KEYBOARD_BACKLIGHT_DVICE = "/sys/class/leds/system76_acpi::kbd_backlight"
+def main():
+    battery = Battery()
+    kb_backlight = KeyboardBacklight(mode='static', battery_handler=battery)
+    kb_backlight.run()
 
 
 if __name__ == '__main__':
-    pass
-"""
-do 
-	while [ $brightness -lt 155 ] 
-	   do
-		echo $brightness > $backlight/brightness
-		brightness=$(( $brightness+1 ))
-	done
-
-	while [ $brightness -gt 15 ]
-       	do
-		echo $brightness > $backlight/brightness
-		brightness=$(( $brightness-1 ))
-	done
-	full=$(cat /sys/class/power_supply/BAT0/charge_full)
-	current=$(cat /sys/class/power_supply/BAT0/charge_now)
-	battery=$(( ($current*100)/$full ))
-
-	if [ $battery -lt 20 ]
-	then echo $red > $backlight/color
-	elif [ $battery -lt 50 ]
-	then echo $yellow > $backlight/color
-	elif [ $battery -lt 100 ]
-	then echo $green > $backlight/color
-	fi
-done
-"""
+    main()
