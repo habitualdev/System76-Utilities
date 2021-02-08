0e87f9a402729efa2e402cc0b5dcd94d2f5c36a6
Jeff Labonte <grimsleepless@protonmail.com>
2021-02-07 19:07:46 -0500

    Update battery to be an instance

--- OryxPro2020/battery-backlight/battery.py
+++ OryxPro2020/battery-backlight/battery.py
@@ -5,17 +5,14 @@ class Battery:
     FULL_BATTERY_PATH = '/sys/class/power_supply/BAT0/charge_full'
     CURRENT_BATTERY_PATH = '/sys/class/power_supply/BAT0/charge_now'
 
-    @classmethod
-    def _read_current_battery(cls):
-        return read_file(cls.CURRENT_BATTERY_PATH)
+    def _read_current_battery(self):
+        return read_file(self.CURRENT_BATTERY_PATH)
 
-    @classmethod
-    def _read_full_battery(cls):
-        return read_file(cls.FULL_BATTERY_PATH)
+    def _read_full_battery(self):
+        return read_file(self.FULL_BATTERY_PATH)
 
-    @classmethod
-    def get_battery_level(cls):
-        current = int(cls._read_current_battery())
-        full = int(cls._read_full_battery())
+    def get_battery_level(self):
+        current = int(self._read_current_battery())
+        full = int(self._read_full_battery())
 
         return (current * 100) // full
